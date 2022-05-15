99c2f86572b2faaf5a162724f5f289171841046e
TetrisRubik <fpablo960@gmail.com>
2022-05-15 12:17:19 +0100

    Jugador La primera implementación del jugador ha sido creada.

--- funciones/principal.js
+++ funciones/principal.js
@@ -6,3 +6,21 @@ lienzo.width = 1024;
 lienzo.heigth = 576;
 
 ctx.fillRect(0, 0, lienzo.width, lienzo.heigth);
+
+class Objeto {
+	constructor(posición) {
+		this.posición = posición;
+	}
+
+	dibuja() {
+		ctx.fillStyle = 'red';
+		ctx.fillRect(this.posición.x, this.posición.y, 50, 100);
+	}
+}
+
+const jugador = new Objeto({
+	x: 0,
+	y: 0
+});
+
+jugador.dibuja();
