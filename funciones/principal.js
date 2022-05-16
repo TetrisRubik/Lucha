f44cf845913b1860f3b9f6fc9ff4d7682732420a
TetrisRubik <fpablo960@gmail.com>
2022-05-16 13:53:38 +0100

    Enemigo y Colores Objeto rival y colores provisionales en clase "Oobjeto" para distinguirlos.

--- funciones/principal.js
+++ funciones/principal.js
@@ -8,19 +8,26 @@ lienzo.heigth = 576;
 ctx.fillRect(0, 0, lienzo.width, lienzo.heigth);
 
 class Objeto {
-	constructor(posición) {
+	constructor(posición, color) {
 		this.posición = posición;
+		this.color = color;
 	}
 
 	dibuja() {
-		ctx.fillStyle = 'red';
+		ctx.fillStyle = this.color;
 		ctx.fillRect(this.posición.x, this.posición.y, 50, 100);
 	}
 }
 
 const jugador = new Objeto({
-	x: 0,
-	y: 0
-});
+	x: 64,
+	y: 64
+}, "#66F");
+
+const enemigo = new Objeto({
+	x: 910,
+	y: 64
+}, "#F22");
 
 jugador.dibuja();
+enemigo.dibuja();
