5b878c35beca8b98e550b80d7c4c413cde03daa6
TetrisRubik <fpablo960@gmail.com>
2022-05-18 16:48:25 +0100

    Movimiento y Actualización Ahora los objetos tienen velocidad x e y que se actualizará mediante la función "actualiza" que estará incluida en la función "animación" que imprime todos los fotogramas.

--- funciones/principal.js
+++ funciones/principal.js
@@ -2,32 +2,42 @@
 const lienzo = document.getElementById("lienzo");
 const ctx = lienzo.getContext("2d");
 
-lienzo.width = 1024;
-lienzo.heigth = 576;
+lienzo.ancho = 1024;
+lienzo.alto = 576;
 
-ctx.fillRect(0, 0, lienzo.width, lienzo.heigth);
+ctx.fillRect(0, 0, lienzo.ancho, lienzo.alto);
 
 class Objeto {
-	constructor(posición, color) {
+	constructor(posición, color, velocidad) {
 		this.posición = posición;
 		this.color = color;
+		this.velocidad = velocidad;
 	}
 
 	dibuja() {
 		ctx.fillStyle = this.color;
 		ctx.fillRect(this.posición.x, this.posición.y, 50, 100);
 	}
+
+	actualiza() {
+		this.dibuja();
+		this.posición.y += 10;
+	}
 }
 
-const jugador = new Objeto({
-	x: 64,
-	y: 64
-}, "#66F");
+const jugador = new Objeto({ x: 64, y: 64 }, "#66F", { x: 0, y: 0 });
 
-const enemigo = new Objeto({
-	x: 910,
-	y: 64
-}, "#F22");
+const rival = new Objeto({ x: 910, y: 64 }, "#F22", { x: 0, y: 0 });
 
 jugador.dibuja();
-enemigo.dibuja();
+rival.dibuja();
+
+function animación() {
+	window.requestAnimationFrame(animación);
+	ctx.fillStyle = "#000";
+	ctx.fillRect(0, 0, lienzo.ancho, lienzo.alto);
+	jugador.actualiza();
+	rival.actualiza();
+}
+
+animación();
