d8e20b7f7b9c914b9f0bf459c077365e4356c1dc
TetrisRubik <fpablo960@gmail.com>
2022-06-19 02:06:01 +0100

    Caja de Ataque Ahora hay una caja (actualmente visible) en frente de los personajes que emula el área de ataque de estos.

--- funciones/objeto.js
+++ funciones/objeto.js
@@ -8,15 +8,30 @@ export default class Objeto {
 		this.color = color;
 		this.velocidad = { x: 0, y: 0 };
 		this.juego = juego;
+		this.ataque = {
+			color: "#0F0",
+			posición: {
+				x: this.posición.x + 75,
+				y: this.posición.y + this.tamaño.alto - 100
+			},
+			tamaño: {
+				ancho: 300,
+				alto: 50
+			}
+		};
 	}
 
 	dibujar() {
 		definir_estilo(this.color);
 		dibujar_rectángulo(this.posición, this.tamaño);
+		definir_estilo(this.ataque.color);
+		dibujar_rectángulo(this.ataque.posición, this.ataque.tamaño);
 	}
 
 	actualizar() {
 		this.dibujar();
+		this.ataque.posición.x = this.posición.x + 75;
+		this.ataque.posición.y = this.posición.y + this.tamaño.alto - 100;
 		this.posición.x += this.velocidad.x;
 		this.posición.y += this.velocidad.y;
 		if (this.posición.y + this.velocidad.y <= this.juego.suelo) {
